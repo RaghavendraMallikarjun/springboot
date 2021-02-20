2bb8f941a1660fe352fe95ec4e15e6d83971dd35
RaghavendraMallikarjun <raghavm6298@gmail.com>
2021-02-20 18:41:56 +0530

    first-commit

---
+++ src/main/java/com/example/springboot/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.example.springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@CrossOrigin(origins="http://localhost:4000")
+public class HelloWorld {
+	
+	@RequestMapping(method=RequestMethod.GET,path="/hello-world")
+	public String HelloWorldController() {
+		return "hello-world";
+	}	
+	
+	@RequestMapping(method=RequestMethod.GET,path="/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldPathvariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("hello-world,%s",name));
+	}
+    
+}
---
+++ src/main/java/com/example/springboot/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.example.springboot;
+
+public class HelloWorldBean {
+	
+	private String message2;
+
+	public HelloWorldBean(String message2) {
+		// TODO Auto-generated constructor stub
+		this.message2=message2;
+	}
+
+	public void setMessage2(String message2) {
+		this.message2 = message2;
+	}
+
+	public String getMessage2() {
+		return message2;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message2=" + message2 + "]";
+	}
+
+
+}
